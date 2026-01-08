53c535064b75ff887c6ad5bbaf5d0b59cd3cfcb9
simon <si@m.on>
2026-01-08 17:56:35 +0100

    Add keymaps to close stuff

--- init.lua
+++ init.lua
@@ -184,6 +184,10 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+-- Keymaps to close stuff
+vim.keymap.set('n', '<leader>xt', '<Cmd>tabclose<cr>', { desc = 'Exit currrent tab' })
+vim.keymap.set('n', '<leader>xx', '<Cmd>qall<cr>', { desc = 'Exit everything' })
+
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
