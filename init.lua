2fa65e296a814fb84480e429a8a93acf5b568573
simon <si@m.on>
2026-01-18 20:12:14 +0100

    Add rust_analyzer lsp

--- init.lua
+++ init.lua
@@ -724,7 +724,7 @@ require('lazy').setup({
         -- gopls = {},
         pyright = {},
         glsl_analyzer = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
